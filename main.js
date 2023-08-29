9abee0ce2bf04782f974c61544b3b756be987dfc
IbadRehman360 <ibadhashim4@gmail.com>
2023-08-29 18:09:57 +0500

    2nd final update

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-const feedbackContainer = document.querySelector('.feedback-container');
-const thankyou = document.querySelector('.thank-you-container');
 const ratingCircles = document.querySelectorAll('.rating-circle');
-const selectedRatingElement = document.getElementById('selected-rating');
 const submitButton = document.querySelector('.feedback-button');
 
 let selectedRating = null;
@@ -14,20 +11,8 @@ ratingCircles.forEach(ratingElement => {
     })
 
     this.classList.add('selected');
-    selectedRating = this.textContent
-    selectedRatingElement.textContent = selectedRating;
 
 
   })
 })
 
-submitButton.addEventListener('click', function () {
-  
-  if (selectedRating) {
-    feedbackContainer.classList.add('hidden');
-    thankyou.classList.remove('hidden');
-    thankyou.classList.add('show');
-  } else {
-    alert('Please select a rating before submitting.');
-  }
-})
